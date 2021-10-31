5ecf76c9573e6bc0a65dfdec2e94c5c72517bcfc
ms0121 <1132601565@qq.com>
2021-10-31 14:05:29 +0800

    push commit

--- src/main/java/com/liu/git/GitTest.java
+++ src/main/java/com/liu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello, git - 4!");
         System.out.println("master test");
         System.out.println("hot-fix test2");
+        System.out.println("push test");
     }
 }
