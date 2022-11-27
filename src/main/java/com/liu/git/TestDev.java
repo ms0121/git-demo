c12abb8dc0c09f6c2cbc1f11a955a2ca23207083
ms0121 <1132601565@qq.com>
2022-11-27 10:54:57 +0800

    dev添加

---
+++ src/main/java/com/liu/git/TestDev.java
@@ -0,0 +1,7 @@
+package com.liu.git;
+
+public class TestDev {
+    public static void main(String[] args) {
+        System.out.println("dev");
+    }
+}
---
+++ src/main/java/com/liu/git/TestDev1.java
@@ -0,0 +1,7 @@
+package com.liu.git;
+
+public class TestDev1 {
+    public static void main(String[] args) {
+        System.out.println("Dev1");
+    }
+}
